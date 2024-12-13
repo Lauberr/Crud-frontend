b50d329804e798cb2f53be5fbbacef6f5535a003
Lauberr <130068637+Lauberr@users.noreply.github.com>
2024-12-12 21:10:26 -0300

    .

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,17 @@
 export function Card({ user }) {
+  const formatDate = (dateString) => {
+    if (!dateString) return "Data não disponível";
+    const options = { day: "2-digit", month: "short", year: "numeric" };
+    return new Date(dateString).toLocaleDateString("en-US", options).replace(",", "");
+  };
+
   return (
     <tr className="bg-white border-b-8 border-gray-100 hover:bg-yellow-200 transition-colors duration-100">
       <td className="p-7">{user.nome}</td>
       <td className="p-7">{user.email}</td>
       <td className="p-7">{user.celular}</td>
       <td className="p-7">{user.id}</td>
-      <td className="p-7">{user.data_de_admissao}</td>
+      <td className="p-7">{formatDate(user.data_de_admissao)}</td>
 
       <td className="p-7"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#F19E39"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg></td>
       
--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -60,16 +60,32 @@ export function Student() {
   return (
     <div className="w-full h-full overflow-hidden">
       <header className="p-6 flex w-full justify-between">
-        <strong className="text-2xl">Lista de Estudantes</strong>
-        <button
-          className="ml-3 p-2 rounded text-white px-5 bg-yellow-400"
-          onClick={() => setShowModal(true)}
-        >
-          NOVO ESTUDANTE
-        </button>
+        <div className="w-full flex justify-between">
+          <svg width="25"  viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M17.7188 9C17.7188 4.18359 13.8164 0.28125 9 0.28125C4.18359 0.28125 0.28125 4.18359 0.28125 9C0.28125 13.8164 4.18359 17.7187 9 17.7188C13.8164 17.7188 17.7187 13.8164 17.7188 9ZM9 16.5938C4.82695 16.5938 1.40625 13.2152 1.40625 9C1.40625 4.82695 4.78477 1.40625 9 1.40625C13.173 1.40625 16.5938 4.78477 16.5938 9C16.5938 13.173 13.2152 16.5938 9 16.5938ZM10.125 12.375L6.75 9L10.125 5.625L10.125 12.375ZM11.25 5.625C11.25 4.62656 10.0371 4.12031 9.33047 4.83047L5.95547 8.20547C5.51602 8.64492 5.51602 9.35859 5.95547 9.79805L9.33047 13.173C10.0371 13.8797 11.25 13.3805 11.25 12.3785L11.25 5.625Z" fill="#C4C4C4" />
+           </svg>
+          <div className="flex">
+            <input
+              className="mr-4 py-2 px-3 border rounded-md"
+              type="search"
+              placeholder="Pesquisa..."
+            />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="30px" fill="#CCCCCC"><path d="M160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v28q80 20 130 84.5T720-560v280h80v80H160Zm320-300Zm0 420q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80ZM320-280h320v-280q0-66-47-113t-113-47q-66 0-113 47t-47 113v280Z"/></svg>
+          </div>
+        </div>
       </header>
 
       <main className="p-8 py-4 bg-gray-100 w-full h-full">
+      <div className="border-b pb-4 w-full flex justify-between">
+          <strong className="text-2xl">Lista de Estudantes</strong>
+          <button
+            className="ml-3 p-2 rounded text-white px-5 bg-yellow-400"
+            onClick={() => setShowModal(true)}
+          >
+            NOVO ESTUDANTE
+          </button>
+        </div> 
+
         <table className="w-full">
           <thead>
             <tr className="bg-gray-100">
