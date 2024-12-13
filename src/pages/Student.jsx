5a7db2321b0bb113a22044d1c9152bc95df35ad1
OttGod12 <130059670+OttGod12@users.noreply.github.com>
2024-12-12 21:31:13 -0300

    botão voltar

--- src/pages/Student.jsx
+++ src/pages/Student.jsx
@@ -1,6 +1,8 @@
 import { Card } from "../components/Card";
 import React, { useEffect, useState } from "react";
 import { ModalForm } from "../components/ModalForm"; // Importe o ModalForm
+import { NavLink } from "react-router-dom";
+
 
 export function Student() {
   const [users, setUsers] = useState([]); // Estado para armazenar os usuários
@@ -61,9 +63,13 @@ export function Student() {
     <div className="w-full h-full overflow-hidden">
       <header className="p-6 flex w-full justify-between">
         <div className="w-full flex justify-between">
-          <svg width="25"  viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M17.7188 9C17.7188 4.18359 13.8164 0.28125 9 0.28125C4.18359 0.28125 0.28125 4.18359 0.28125 9C0.28125 13.8164 4.18359 17.7187 9 17.7188C13.8164 17.7188 17.7187 13.8164 17.7188 9ZM9 16.5938C4.82695 16.5938 1.40625 13.2152 1.40625 9C1.40625 4.82695 4.78477 1.40625 9 1.40625C13.173 1.40625 16.5938 4.78477 16.5938 9C16.5938 13.173 13.2152 16.5938 9 16.5938ZM10.125 12.375L6.75 9L10.125 5.625L10.125 12.375ZM11.25 5.625C11.25 4.62656 10.0371 4.12031 9.33047 4.83047L5.95547 8.20547C5.51602 8.64492 5.51602 9.35859 5.95547 9.79805L9.33047 13.173C10.0371 13.8797 11.25 13.3805 11.25 12.3785L11.25 5.625Z" fill="#C4C4C4" />
-           </svg>
+
+          <NavLink to="/" className="ml-3 p-2 rounded text-white px-5">
+                <svg width="25" height="25" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M17.7188 9C17.7188 4.18359 13.8164 0.28125 9 0.28125C4.18359 0.28125 0.28125 4.18359 0.28125 9C0.28125 13.8164 4.18359 17.7187 9 17.7188C13.8164 17.7188 17.7187 13.8164 17.7188 9ZM9 16.5938C4.82695 16.5938 1.40625 13.2152 1.40625 9C1.40625 4.82695 4.78477 1.40625 9 1.40625C13.173 1.40625 16.5938 4.78477 16.5938 9C16.5938 13.173 13.2152 16.5938 9 16.5938ZM10.125 12.375L6.75 9L10.125 5.625L10.125 12.375ZM11.25 5.625C11.25 4.62656 10.0371 4.12031 9.33047 4.83047L5.95547 8.20547C5.51602 8.64492 5.51602 9.35859 5.95547 9.79805L9.33047 13.173C10.0371 13.8797 11.25 13.3805 11.25 12.3785L11.25 5.625Z" fill="#C4C4C4" />
+                </svg> 
+          </NavLink>
+
           <div className="flex">
             <input
               className="mr-4 py-2 px-3 border rounded-md"
